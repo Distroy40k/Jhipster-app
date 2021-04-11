0c03c93e917c77efb849568daf12ef179c99bc70
Jules Gonzalez <julesgonzalez99@yahoo.fr>
2021-04-11 18:53:59 +0200

    Ajout des entités avec JDL

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'blog',
+        loadChildren: () => import('./blog/blog.module').then(m => m.MyAppBlogModule),
+      },
+      {
+        path: 'post',
+        loadChildren: () => import('./post/post.module').then(m => m.MyAppPostModule),
+      },
+      {
+        path: 'tag',
+        loadChildren: () => import('./tag/tag.module').then(m => m.MyAppTagModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
